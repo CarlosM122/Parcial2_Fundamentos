c0b9c25b882026d641201e8af88b29b6d0096662
CarlosM122 <141981976+CarlosM122@users.noreply.github.com>
2024-10-28 01:36:32 -0500

    Update calculadora.asm

--- calculadora.asm
+++ calculadora.asm
@@ -1,305 +1,182 @@
 section .data
- 
    ; Mensajes
- 
-   msg1      db      10,'-Calculadora-',10,0
-   lmsg1      equ      $ - msg1
- 
-   msg2      db      10,'Numero 1: ',0
-   lmsg2      equ      $ - msg2
- 
-   msg3      db      'Numero 2: ',0
-   lmsg3      equ      $ - msg3
- 
-   msg4      db      10,'1. Sumar',10,0
-   lmsg4      equ      $ - msg4
- 
-   msg5      db      '2. Restar',10,0
-   lmsg5      equ      $ - msg5
- 
-   msg6      db      '3. Multiplicar',10,0
-   lmsg6      equ      $ - msg6
- 
-   msg7      db      '4. Dividir',10,0
-   lmsg7      equ      $ - msg7
- 
-   msg8      db      'Operacion: ',0
-   lmsg8      equ      $ - msg8
- 
-   msg9      db      10,'Resultado: ',0
-   lmsg9      equ      $ - msg9
- 
-   msg10      db      10,'Opcion Incorrecta',10,0
-   lmsg10      equ      $ - msg10
- 
-   nlinea      db      10,10,0
-   lnlinea      equ      $ - nlinea
- 
+   msg1 db 10, '-Calculadora-', 10, 0
+   msg2 db 10, 'Numero 1: ', 0
+   msg3 db 'Numero 2: ', 0
+   msg4 db 10, '1. Sumar', 10, '2. Restar', 10, '3. Multiplicar', 10, '4. Dividir', 10, 0
+   msg5 db 'Operacion: ', 0
+   msg6 db 10, 'Resultado: ', 0
+   error_div0 db 10, 'Error: Division por cero', 10, 0
+   error_op db 10, 'Opcion Incorrecta', 10, 0
+   newline db 10, 10, 0
+
 section .bss
- 
-   ; Espacios reservados para almacenar los valores proporcionados
-   ; por el usuario.
- 
-   opc         resb    2
-   num1      resw   2
-   num2      resw    2
-   result      resw    2
- 
+   ; Espacios reservados
+   opc resb 1
+   num1 resb 5
+   num2 resb 5
+   result resb 10
+
 section .text
- 
    global _start
- 
+
 _start:
- 
-   ; Imprimimos en pantalla el mensaje 1
+   ; Mostrar mensajes
    mov eax, 4
    mov ebx, 1
    mov ecx, msg1
-   mov edx, lmsg1
-   int 80h
- 
-   ; Imprimimos en pantalla el mensaje 2
+   mov edx, 17
+   int 0x80
+
+   ; Obtener num1
    mov eax, 4
    mov ebx, 1
    mov ecx, msg2
-   mov edx, lmsg2
-   int 80h
- 
-   ; Obtenemos el valor de numero1
+   mov edx, 10
+   int 0x80
    mov eax, 3
    mov ebx, 0
    mov ecx, num1
-   mov edx, 2
-   int 80h
- 
-   ; Imprimimos en pantalla el mensaje 3
+   mov edx, 5
+   int 0x80
+
+   ; Obtener num2
    mov eax, 4
    mov ebx, 1
    mov ecx, msg3
-   mov edx, lmsg3
-   int 80h
- 
-   ; Obtenemos el valor de numero2
+   mov edx, 10
+   int 0x80
    mov eax, 3
    mov ebx, 0
    mov ecx, num2
-   mov edx, 2
-   int 80h
- 
-   ; Imprimimos en pantalla el mensaje 4
+   mov edx, 5
+   int 0x80
+
+   ; Mostrar opciones
    mov eax, 4
    mov ebx, 1
    mov ecx, msg4
-   mov edx, lmsg4
-   int 80h
- 
-   ; Imprimimos en pantalla el mensaje 5
+   mov edx, 36
+   int 0x80
+
+   ; Obtener operación
    mov eax, 4
    mov ebx, 1
    mov ecx, msg5
-   mov edx, lmsg5
-   int 80h
- 
-   ; Imprimimos en pantalla el mensaje 6
+   mov edx, 11
+   int 0x80
+   mov eax, 3
+   mov ebx, 0
+   mov ecx, opc
+   mov edx, 1
+   int 0x80
+
+   ; Convertir num1 y num2 a enteros
+   call str_to_int
+   mov esi, eax ; Guardar num1 en esi
+   call str_to_int
+   mov edi, eax ; Guardar num2 en edi
+
+   ; Procesar operación
+   mov al, [opc]
+   sub al, '0'
+
+   cmp al, 1
+   je suma
+   cmp al, 2
+   je resta
+   cmp al, 3
+   je multiplicacion
+   cmp al, 4
+   je division
+
+   ; Mostrar mensaje de opción incorrecta
+   mov eax, 4
+   mov ebx, 1
+   mov ecx, error_op
+   mov edx, 20
+   int 0x80
+   jmp finalizar
+
+suma:
+   add esi, edi
+   jmp mostrar_resultado
+
+resta:
+   sub esi, edi
+   jmp mostrar_resultado
+
+multiplicacion:
+   imul esi, edi
+   jmp mostrar_resultado
+
+division:
+   cmp edi, 0
+   je error_division_cero
+   xor edx, edx
+   idiv edi
+   jmp mostrar_resultado
+
+error_division_cero:
+   mov eax, 4
+   mov ebx, 1
+   mov ecx, error_div0
+   mov edx, 24
+   int 0x80
+   jmp finalizar
+
+mostrar_resultado:
+   ; Convertir resultado a string
+   mov eax, esi
+   call int_to_str
+   mov ecx, result
+
+   ; Mostrar mensaje de resultado
    mov eax, 4
    mov ebx, 1
    mov ecx, msg6
-   mov edx, lmsg6
-   int 80h
- 
-   ; Imprimimos en pantalla el mensaje 7
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, msg7
-   mov edx, lmsg7
-   int 80h
- 
-   ; Imprimimos en pantalla el mensaje 8
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, msg8
-   mov edx, lmsg8
-   int 80h
- 
-   ; Obtenemos la opcion deseada por el usuario
-   mov ebx,0
-   mov ecx,opc
-   mov edx,2
-   mov eax,3
-   int 80h
- 
-   mov ah, [opc]   ; Movemos la opcion seleccionada al registro ah
-   sub ah, '0'      ; Convertimos de ascii a decimal
- 
-   ; Comparamos el valor ingresado por el usuario para saber que
-   ; operacion desea realizar.
-   cmp ah, 1
-   je sumar
-   cmp ah, 2
-   je restar
-   cmp ah, 3
-   je multiplicar
-   cmp ah, 4
-   je dividir
- 
-   ; Si el valor ingresado por el usuario no cumple ninguna de las
-   ; condiciones anteriores entonces mostramos un mensaje de error y
-   ; finalizamos el programa.
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, msg10
-   mov edx, lmsg10
-   int 80h
- 
-   jmp salir
- 
-sumar:
-   ; Guardamos los numeros en los registros eax y ebx
-   mov eax, [num1]
-   mov ebx, [num2]
- 
-   ; Conversion de ascii a decimal
-   sub eax, '0'
-   sub ebx, '0'
- 
-   ; Suma
-   add eax, ebx
- 
-   ; Conversion de decimal a ascii
-   add eax, '0'
- 
-   ; Movemos el resultado
-   mov [result], eax
- 
-   ; Imprimimos el mensaje 9
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, msg9
-   mov edx, lmsg9
-   int 80h
- 
-   ; Imprimimos el resultado
+   mov edx, 12
+   int 0x80
    mov eax, 4
    mov ebx, 1
    mov ecx, result
-   mov edx, 1
-   int 80h
- 
-   ; Finalizamos el programa
-   jmp salir
- 
-restar:
-   ; Guardamos los numeros en los registros eax y ebx
-   mov eax, [num1]
-   mov ebx, [num2]
- 
-   ; Conversion de ascii a decimal
-   sub eax, '0'
-   sub ebx, '0'
- 
-   ; Resta
-   sub eax, ebx
- 
-   ; Conversion de decimal a ascii
-   add eax, '0'
- 
-   ; Movemos el resultado
-   mov [result], eax
- 
-   ; Imprimimos el mensaje 9
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, msg9
-   mov edx, lmsg9
-   int 80h
- 
-   ; Imprimimos el resultado
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, result
-   mov edx, 1
-   int 80h
- 
-   ; Finalizamos el programa
-   jmp salir
- 
-multiplicar:
-   ; Guardamos los numeros en los registros ax y bx
-   mov ax, [num1]
-   mov bx, [num2]
- 
-   ; Conversion de ascii a decimal
-   sub ax, '0'
-   sub bx, '0'
- 
-   ; Multiplicacion. AL = AX x BX
-   mul bx
- 
-   ; Conversion decimal a ascii
-   add al, '0'
- 
-   ; Movemos el resultado
-   mov [result], al
- 
-   ; Imprimimos el mensaje 9
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, msg9
-   mov edx, lmsg9
-   int 80h
- 
-   ; Imprimimos el resultado
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, result
-   mov edx, 1
-   int 80h
- 
-   ; Finalizamos el programa
-   jmp salir
- 
-dividir:
-	; Guardamos los numeros en los registros ax y bl
-	mov dx, 0
-	mov ax, [num1]
-	mov bx, [num2]
+   int 0x80
 
-	; Conversion de ascii a decimal
-	sub ax, '0'
-	sub bx, '0'
-	; Division. AX = DX:AX / BX
-	div bx
-
-	; Conversion decimal a ascii
-	add ax, '0'
-	; Movemos el resultado
-	mov [result], ax
-
-	; Imprimimos el mensaje 9
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, msg9
-	mov edx, lmsg9
-	int 80h
-
-	; Imprimimos el resultado
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, result
-	mov edx, 1
-	int 80h
-
-	; Finalizamos el programa
-	jmp salir
- 
-salir:
-   ; Imprimimos dos nuevas lineas
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, nlinea
-   mov edx, lnlinea
-   int 80h
-   ; Finalizamos el programa
+finalizar:
+   ; Salir del programa
    mov eax, 1
    mov ebx, 0
-   int 80h
+   int 0x80
+
+; Convierte string a integer
+str_to_int:
+   xor eax, eax
+   xor ebx, ebx
+   mov ecx, 0
+str_to_int_loop:
+   mov bl, byte [ecx + num1]
+   cmp bl, 10
+   je str_to_int_done
+   sub bl, '0'
+   imul eax, eax, 10
+   add eax, ebx
+   inc ecx
+   jmp str_to_int_loop
+str_to_int_done:
+   ret
+
+; Convierte integer a string
+int_to_str:
+   mov edi, result + 9
+   mov byte [edi], 0
+   mov ecx, 10
+int_to_str_loop:
+   xor edx, edx
+   div ecx
+   add dl, '0'
+   dec edi
+   mov [edi], dl
+   test eax, eax
+   jnz int_to_str_loop
+   mov ecx, edi
+   ret
